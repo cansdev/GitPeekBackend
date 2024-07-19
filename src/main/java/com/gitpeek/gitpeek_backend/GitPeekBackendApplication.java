32f598c9e05900937ed81db5f5bfabc85cc2ad98
can.sari <can.sari@TUR066P22S.local>
2024-07-19 11:19:57 +0300

    Initial Commit

---
+++ src/main/java/com/gitpeek/gitpeek_backend/GitPeekBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gitpeek.gitpeek_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPeekBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPeekBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpeek/gitpeek_backend/GitPeekBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpeek.gitpeek_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPeekBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
